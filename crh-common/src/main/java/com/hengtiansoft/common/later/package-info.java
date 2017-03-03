ba370ce202c1be88843f2e9c006eafe2081f9cbf
yiminli <yiminli@hengtiansoft.com>
2017-03-03 15:43:02 +0800

    update  code

--- crh-common/src/main/java/com/hengtiansoft/common/later/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by liminghua on 15/12/9.
- */
-package com.hengtiansoft.common.later;
--- crh-common/src/main/java/com/hengtiansoft/common/later/thread/LaterThread.java
+++
@@ -1,8 +0,0 @@
-package com.hengtiansoft.common.later.thread;
-
-/**
- * Created by liminghua on 16/1/7.
- */
-public interface LaterThread {
-    void run(Runnable runnable);
-}
