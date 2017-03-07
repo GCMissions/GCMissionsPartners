2252e64f851d29414873ce899ce5a7a0d1c5019d
yiminli <yiminli@hengtiansoft.com>
2017-03-07 15:02:41 +0800

    update code

--- crh-common/src/main/java/com/hengtiansoft/church/dao/CountryDao.java
+++ crh-common/src/main/java/com/hengtiansoft/church/dao/CountryDao.java
@@ -11,6 +11,6 @@ import com.hengtiansoft.church.entity.CountryEntity;
 public interface CountryDao extends JpaRepository<CountryEntity, Long>,
     JpaSpecificationExecutor<CountryEntity>{
 
-    @Query(value = "select c.* from country c left join country_region_ref gr on c.ID = gr.COUNTRY_ID where gr.REGION_ID = ?1 and c.del_flag = '1'", nativeQuery = true)
+    @Query(value = "select c.* from country c left join country_region_ref gr on c.ID = gr.COUNTRY_ID where gr.REGION_ID = ?1 and c.del_flag = '1' and gr.del_flag = '1'", nativeQuery = true)
     List<CountryEntity> findAllCountryByGroupId(Long groupId);
 }
--- crh-common/src/main/java/com/hengtiansoft/church/dao/PartnerDao.java
+++ crh-common/src/main/java/com/hengtiansoft/church/dao/PartnerDao.java
@@ -11,6 +11,6 @@ import com.hengtiansoft.church.entity.PartnersEntity;
 public interface PartnerDao extends JpaRepository<PartnersEntity, Long>,
     JpaSpecificationExecutor<PartnersEntity>{
 
-    @Query(value = "select p.* from partners p left join country_region_ref gr on p.C_R_REF_ID = gr.ID where gr.REGION_ID = ?1 and gr.COUNTRY_ID = ?2 and p.DEL_FLAG = '1'", nativeQuery = true)
+    @Query(value = "select p.* from partners p left join country_region_ref gr on p.C_R_REF_ID = gr.ID where gr.REGION_ID = ?1 and gr.COUNTRY_ID = ?2 and p.DEL_FLAG = '1' and gr.del_flag = '1'", nativeQuery = true)
     List<PartnersEntity> getAllPartnersByRegionIdAndCountryId(Long regionId, Long countryId);
 }
